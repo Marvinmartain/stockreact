656758d9d442b1851bd1793539dcafda0e1d0b05
Marvinmartain <122340360+Marvinmartain@users.noreply.github.com>
2023-04-21 18:48:13 -0400

    first commit

---
+++ stockreact/src/components/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>Stock Values</div>
+  )
+}
+
+export default About
---
+++ stockreact/src/index.js
@@ -0,0 +1,19 @@
+
+import ReactDOM from "react-dom";
+import "./App.css";
+import App from "./App";
+//IMPORT BrowserRouter and rename it to Router
+import { BrowserRouter as Router } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+    <Router>
+      <App />
+    </Router>
+
+  
+);
+
+
+
